c5fe25e80e138a091231a6bbf089d1c016ade190
Ime Emmanuel <emmanuel84y@gmail.com>
2026-01-30 16:56:21 +0100

    Add files via upload

---
+++ app/case-studies/page.tsx
@@ -0,0 +1,11 @@
+
+'use client';
+
+import React from 'react';
+import CaseStudiesView from '../../components/CaseStudiesView';
+import { useRouter } from 'next/navigation';
+
+export default function CaseStudiesPage() {
+  const router = useRouter();
+  return <CaseStudiesView onGoBack={() => router.push('/')} />;
+}
